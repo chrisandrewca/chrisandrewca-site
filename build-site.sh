0fe082b2918bd8f4fd5a80c9cfcf7049801eeebb
chrisandrewca <chris.andrew.ca@gmail.com>
2020-08-07 12:20:40 -0700

    Try jpg and http/2

--- build-site.sh
+++ build-site.sh
@@ -3,4 +3,4 @@ rm -rf ./result
 mkdir result
 npx postcss --config . ../src/index.css > ./result/index.css
 cp ../src/index.html ./result/index.html
-cp ../src/chrisandrewca.gif ./result/chrisandrewca.gif
+cp ../src/chrisandrewca.jpg ./result/chrisandrewca.jpg
