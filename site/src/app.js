850b69f7b2f3634ba2ba1a8cac726e3ff59bcbc2
chrisandrewca <chris.andrew.ca@gmail.com>
2020-08-09 06:14:39 +0000

    Product and API stub

--- site/src/app.js
+++ site/src/app.js
@@ -1,7 +1,18 @@
 import { html, render } from 'lit-html';
 
 const App = (document) => {
-  const app = html`<p>Hello, world!</p>`;
+  const app = html`
+    <picture>
+      <source type="image/webp" media="(min-width: 450px)" srcset="/media/512/chrisandrewca.webp" />
+      <source type="image/jpeg" media="(min-width: 450px)" srcset="/media/512/chrisandrewca.jpg" />
+      <source type="image/webp" media="(min-width: 321px)" srcset="/media/375/chrisandrewca.webp" />
+      <source type="image/jpeg" media="(min-width: 321px)" srcset="/media/375/chrisandrewca.jpg" />
+      <img src="/media/270/chrisandrewca.webp" /><!-- TODO fix me -->
+    </picture>
+    <div>
+      <a href='#'>Buy my T-Shirt</a>
+    </div>`;
+
   render(app, document.body);
 };
 
---
+++ site/src/product.js
@@ -0,0 +1,10 @@
+import { html, render } from 'lit-html';
+
+const Product = () => {
+  const markup = html`
+    <h1>Product page</h1>`;
+
+  render(markup, ???); // return?
+};
+
+export default Product;
